43eed5bacc1a9ae771bf47685cd19f92d3c3ad43
nabijaczleweli <nabijaczleweli@gmail.com>
2015-12-12 19:00:27 +0100

    Handle simple clients POSTing ChatMessages, with error handling. Add more fields to Cargo.toml

--- src/client_handler.rs
+++ src/client_handler.rs
@@ -1,9 +1,35 @@
+use chattium_oxide_lib::json::FromJsonnable;
+use chattium_oxide_lib::ChatMessage;
 use hyper::server::{Request, Response};
 use hyper::status::StatusCode;
+use hyper::method::Method;
+use std::io::Read;
 
 
 pub fn handle_client(req: Request, mut res: Response) {
-	match req.method {
-	  _ => *res.status_mut() = StatusCode::ImATeapot,
+	let mut req = req;
+	*res.status_mut() = match req.method {
+		Method::Post => {
+			let mut reqbody = String::new();
+			match req.read_to_string(&mut reqbody) {
+				Ok(_) =>
+					match ChatMessage::from_json_string(&reqbody) {
+						Ok(mut message) => {
+							message.sender.fill_ip(req.remote_addr);
+							println!("{:?}", message);
+							StatusCode::Ok
+						},
+						Err(error) => {
+							println!("Couldn't process POSTed message: {}", error);
+							StatusCode::UnprocessableEntity
+						},
+					},
+				Err(error) => {
+					println!("Failed reading request from {}: {}", req.remote_addr, error);
+					StatusCode::UnsupportedMediaType  // non-UTF-8
+				},
+			}
+		},
+	  _ => StatusCode::ImATeapot,
 	}
 }
