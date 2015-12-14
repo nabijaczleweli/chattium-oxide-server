e2303ea55b4162115d648455cd3786cacbd49037
nabijaczleweli <nabijaczleweli@gmail.com>
2015-12-14 14:53:39 +0100

    Add commandlne options and SSL support. Add example config, key and cert

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,47 @@
 extern crate chattium_oxide_lib;
+extern crate yaml_file_handler;
 extern crate hyper;
+extern crate clap;
 
 mod client_handler;
+mod options;
 
+use options::Options;
+use hyper::Error as HyperError;
+use hyper::net::{Openssl, NetworkListener};
 use hyper::server::Server;
 
 
+fn handle_server<L: 'static + NetworkListener + Send>(server: Server<L>) {
+	match server.handle(client_handler::handle_client) {
+		Ok(listener) => println!("Listening on {}", listener.socket),
+		Err(error) => println!("Couldn't handle client: {}", error),
+	}
+}
+
 fn main() {
-	match Server::http("127.0.0.1:50030") {
-		Ok(server) =>
-			match server.handle(client_handler::handle_client) {
-				Ok(listener) => println!("Listening on {}", listener.socket),
-				Err(error) => println!("Couldn't handle client: {}", error),
-			},
-		Err(error) => println!("Couldn't start server: {}", error),
+	let options = Options::parse();
+	println!("{:?}", options);
+
+	let addr = &format!("127.0.0.1:{}", options.port)[..];
+	match options.ssl {
+		Some(pair) => {
+			match Openssl::with_cert_and_key(pair.0, pair.1) {
+				Ok(ssl) => {handle_server_error(Server::https(&addr, ssl)).map(handle_server);},
+				Err(error) => println!("Couldn't set up OpenSSL: {}", error),
+			}
+		},
+		None => {handle_server_error(Server::http(&addr)).map(handle_server);},
+	};
+}
+
+
+fn handle_server_error<S>(res: Result<S, HyperError>) -> Option<S> {
+	match res {
+		Ok(server) => Some(server),
+		Err(error) => {
+			println!("Couldn't start server: {}", error);
+			None
+		},
 	}
 }
---
+++ src/options.rs
@@ -0,0 +1,127 @@
+use clap::{App as Clapp, Arg as Clarg};
+use std::io::{self, Write};
+use std::str::FromStr;
+use std::fmt::Arguments as FormatArguments;
+use std::path::PathBuf;
+use std::error::Error;
+use yaml_file_handler::Yaml;
+use yaml_file_handler::yaml_handler::FileHandler as YamlFileHandler;
+
+
+#[derive(Debug)]
+pub struct Options {
+	pub port: u16,
+	pub ssl: Option<(String, String)>,
+}
+
+
+impl Options {
+	/// Parses commandline arguments into an [`Options`](#) instance
+	///
+	/// Optionally reads from a config file in [YAML](http://yaml.org) format, however commandline arguments take preference thereover.
+	/// The config file format is non-trivial, see `"example/config.yml"`.
+	pub fn parse() -> Options {
+		const PORT_USAGE: &'static str = "-p --port [port] 'Specifies the port the server will run on, will prompt if not specified'";
+		const SSL_USAGE: &'static str = "--ssl [ssl] 'Sets SSL cert and key in the format: \"CERT_PATH;CERT_KEY_PATH\"'";
+		const USAGE: &'static str = "-c --config=[conf] 'Sets config file to load, values will be overriden by commandline args'";
+
+		let matches = Clapp::new("chattium-oxide-server").version("0.1.0")
+		                                                 .author("nabijaczleweli <nabijaczleweli@gmail.com>")
+		                                                 .about("Chat server for chattium-oxide-client")
+		                                                 .args_from_usage(USAGE)
+		                                                 .arg(Clarg::from_usage(PORT_USAGE).validator(|val| {
+		                                                 	match u16::from_str(&*&val) {
+		                                                 		Ok(_) => Ok(()),
+		                                                 		Err(error) => Err(error.description().to_string()),
+		                                                 	}
+		                                                 }))
+		                                                 .arg(Clarg::from_usage(SSL_USAGE).validator(|val| {
+		                                                 	if val.chars().find(|c| *c == ';').is_some() {
+		                                                 		if val.len() >= 3 {  // Naive, replace with something better, maybe?
+		                                                 			Ok(())
+		                                                 		} else {
+		                                                 			Err(String::from("the value must contain 2 paths"))
+		                                                 		}
+		                                                 	} else {
+		                                                 		Err(String::from("the value must contain the separator"))
+		                                                 	}
+		                                                 }))
+		                                                 .get_matches();
+		let mut port: Option<u16> = None;
+		let mut ssl_cert: Option<String> = None;
+		let mut ssl_key: Option<String> = None;
+		if let Some(conf) = matches.value_of("conf") {
+			let mut yaml = YamlFileHandler::new();
+			if yaml.add_files(vec![conf]) {
+				if let Some(yaml) = yaml.read_all_files().as_ref().map(|all| {
+					let mut b = PathBuf::from(conf);
+					b.set_extension("");
+					&all[b.file_name().unwrap().to_str().unwrap()]
+				}) {
+					if let Some(map) = yaml["ssl"].as_hash() {
+						ssl_cert = if let Some(cert) = map.get(&Yaml::String("cert".to_string())) {cert.as_str().map(|c| c.to_string())} else {None};
+						ssl_key  = if let Some(key)  = map.get(&Yaml::String("key".to_string()))  {key .as_str().map(|k| k.to_string())} else {None};
+					}
+					port = yaml["port"].as_i64().map(|p| p as u16);
+				}
+			}
+		}
+
+		if let Some(cport) = matches.value_of("port") {port = Some(u16::from_str(cport).unwrap())}  // Validated using Arg::validator
+		if let Some(cssl) = matches.value_of("ssl") {
+			let mut paths = cssl.split(';');
+			ssl_cert = Some(paths.next().unwrap().to_string());
+			ssl_key  = Some(paths.next().unwrap().to_string());
+		}
+
+		if port.is_none() {
+			let mut tport: Option<u16> = None;
+			while tport.is_none() {
+				match read_prompted(format_args!("No port specified.\nPlease type in the port now: ")) {
+					Ok(rport) =>
+						match rport {
+							Some(rport) =>
+								match u16::from_str(&*&rport) {
+									Ok(port) => tport = Some(port),
+									Err(error) => println!("Invalid port value: {}", error),
+								},
+							None => (),
+						},
+					Err(error) => println!("Couldn't read port: {}", error),
+				}
+			}
+			port = tport;
+		}
+
+		assert!(port.is_some());
+		Options{
+			port: port.unwrap(),
+			ssl: match (ssl_cert, ssl_key) {
+				(Some(cert), Some(key)) => Some((cert, key)),
+				(None, Some(key)) => {
+					println!("SSL key specified (\"{}\"), but certificate not specified", key);
+					None
+				},
+				(Some(cert), None) => {
+					println!("SSL key not specified, but certificate specified (\"{}\")", cert);
+					None
+				},
+				(None, None) => None,
+			},
+		}
+	}
+}
+
+
+fn read_prompted(prompt: FormatArguments) -> io::Result<Option<String>> {
+	try!(io::stdout().write_fmt(prompt));
+	try!(io::stdout().flush());
+
+	let mut obuf = String::new();
+	try!(io::stdin().read_line(&mut obuf));  // Don't match on this directly, because it returns with "\r\n"
+	let obuf = obuf.trim();
+	Ok(match obuf.len() {
+		0 => None,
+		_ => Some(obuf.trim().to_string()),
+	})
+}
