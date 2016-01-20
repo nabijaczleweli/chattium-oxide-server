64e503e55ac79152526ea80085b57edfa5abd6d4
nabijaczleweli <nabijaczleweli@gmail.com>
2016-01-20 15:35:07 +0100

    Add encoding algorithm selection

--- src/html_response_handler.rs
+++ src/html_response_handler.rs
@@ -1,3 +1,4 @@
+use hyper::header::{Headers, Encoding, AcceptEncoding};
 use regex::Regex;
 
 
@@ -24,5 +25,31 @@ impl HtmlResponseHandler {
 		];
 
 		regices.iter().fold(what.to_string(), |curr, ref tpl| tpl.0.replace_all(&curr[..], &tpl.1[..]))
+	}
+
+	fn encode_algo(headers: &Headers) -> Encoding {
+		match headers.get::<AcceptEncoding>() {
+			Some(&AcceptEncoding(ref encodings)) => {
+				let mut encodings = encodings.clone();
+				encodings.sort_by(|ref qil, ref qir| qil.quality.cmp(&qir.quality));
+
+				let mut ret = Encoding::Identity;
+				for enc in encodings {
+					match enc.item {
+						Encoding::Gzip => {
+							ret = Encoding::Gzip;
+							break;
+						},
+						Encoding::Deflate => {
+							ret = Encoding::Deflate;
+							break;
+						},
+						_ => (),
+					}
+				}
+				ret
+			},
+			None => Encoding::Identity,
+		}
 	}
 }
