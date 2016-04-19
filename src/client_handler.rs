1026aac97f7d9f2373564e99d93f60cc572e4762
nabijaczleweli <nabijaczleweli@gmail.com>
2016-04-19 23:43:06 +0200

    Fix name in LICENSE

--- src/client_handler.rs
+++ src/client_handler.rs
@@ -1,7 +1,8 @@
 use chattium_oxide_lib::json::{FromJsonnable, ToJsonnable};
 use chattium_oxide_lib::ChatMessage;
 use hyper::server::{Handler, Request, Response};
-use hyper::header::{ContentLength, ContentType, CacheControl, CacheDirective, ContentLanguage, Server, qitem};
+use hyper::header::{ContentLength, ContentType, CacheControl, CacheDirective, ContentLanguage,
+                    Server, qitem};
 use hyper::status::StatusCode;
 use hyper::method::Method;
 use hyper::LanguageTag;
@@ -13,14 +14,14 @@ use std::io::{self, Read, Write, stderr};
 
 
 pub struct ClientHandler {
-	messages    : RwLock<Vec<ChatMessage>>,
-	message_id  : RwLock<u64>,
-	html_message: String,
+    messages: RwLock<Vec<ChatMessage>>,
+    message_id: RwLock<u64>,
+    html_message: String,
 }
 
 impl ClientHandler {
-	pub fn new() -> ClientHandler {
-		ClientHandler{
+    pub fn new() -> ClientHandler {
+        ClientHandler{
 			messages    : RwLock::new(Vec::new()),
 			message_id  : RwLock::new(1),
 			html_message: Self::compact(r##"<!DOCTYPE html>
@@ -64,52 +65,64 @@ impl ClientHandler {
 			                                </body>
 			                                </html>"##),
 		}
-	}
+    }
 
 
-	fn compact(what: &str) -> String {
-		let regices = [
-			(Regex::new(r#"\s+"#).unwrap(), " "),
-			(Regex::new(r#">\s<"#).unwrap(), "><"),
-			(Regex::new(r#"\s/>"#).unwrap(), "/>"),
-			(Regex::new(r#"\s?\{\s?"#).unwrap(), "{"),
-			(Regex::new(r#"\s?\}\s?"#).unwrap(), "}"),
-		];
+    fn compact(what: &str) -> String {
+        let regices = [(Regex::new(r#"\s+"#).unwrap(), " "),
+                       (Regex::new(r#">\s<"#).unwrap(), "><"),
+                       (Regex::new(r#"\s/>"#).unwrap(), "/>"),
+                       (Regex::new(r#"\s?\{\s?"#).unwrap(), "{"),
+                       (Regex::new(r#"\s?\}\s?"#).unwrap(), "}")];
 
-		regices.iter().fold(what.to_string(), |curr, ref tpl| tpl.0.replace_all(&curr[..], &tpl.1[..]))
-	}
+        regices.iter().fold(what.to_string(),
+                            |curr, ref tpl| tpl.0.replace_all(&curr[..], &tpl.1[..]))
+    }
 }
 
 impl Handler for ClientHandler {
-	fn handle(&self, mut req: Request, mut res: Response) {
-		let mut body_ref: Option<&String> = None;
-		let mut body    : Option<String>  = None;
+    fn handle(&self, mut req: Request, mut res: Response) {
+        let mut body_ref: Option<&String> = None;
+        let mut body: Option<String> = None;
 
-		let mut reqbody = String::new();
-		*res.status_mut() = match req.read_to_string(&mut reqbody) {
-			Ok(_) =>
-				match req.method {
-					Method::Post =>
-						match ChatMessage::from_json_string(&reqbody) {
-							Ok(mut message) => {
-								message.sender.fill_ip(req.remote_addr);
-								message.fill_id(self.message_id.write().unwrap());
-								println!("{}@{}: {} @ {} # {}", message.sender.name, req.remote_addr, message.value, strftime("%T", &message.time_posted).unwrap(),
-								                                message.id);
-								self.messages.write().unwrap().push(message);
-								StatusCode::Ok
-							},
-							Err(error) => {
-								let _ = stderr().write_fmt(format_args!("Couldn't process POSTed message from {}: {}\n", req.remote_addr, error));
-								StatusCode::UnprocessableEntity
-							},
-						},
-					Method::Get => {  // Web browser, probably
-						println!("Serving {} HTML message to connect via client.", req.remote_addr);
-						body_ref = Some(&self.html_message);
-						res.headers_mut().set(ContentType::html());
-						res.headers_mut().set(Server(concat!("chattium-oxide-server/", env!("CARGO_PKG_VERSION")).to_string()));
-						res.headers_mut().set(ContentLanguage(vec![qitem(LanguageTag{
+        let mut reqbody = String::new();
+        *res.status_mut() = match req.read_to_string(&mut reqbody) {
+            Ok(_) => {
+                match req.method {
+                    Method::Post => {
+                        match ChatMessage::from_json_string(&reqbody) {
+                            Ok(mut message) => {
+                                message.sender.fill_ip(req.remote_addr);
+                                message.fill_id(self.message_id.write().unwrap());
+                                println!("{}@{}: {} @ {} # {}",
+                                         message.sender.name,
+                                         req.remote_addr,
+                                         message.value,
+                                         strftime("%T", &message.time_posted).unwrap(),
+                                         message.id);
+                                self.messages.write().unwrap().push(message);
+                                StatusCode::Ok
+                            }
+                            Err(error) => {
+                                let _ = stderr().write_fmt(format_args!("Couldn't process \
+                                                                         POSTed message from \
+                                                                         {}: {}\n",
+                                                                        req.remote_addr,
+                                                                        error));
+                                StatusCode::UnprocessableEntity
+                            }
+                        }
+                    }
+                    Method::Get => {
+                        // Web browser, probably
+                        println!("Serving {} HTML message to connect via client.",
+                                 req.remote_addr);
+                        body_ref = Some(&self.html_message);
+                        res.headers_mut().set(ContentType::html());
+                        res.headers_mut().set(Server(concat!("chattium-oxide-server/",
+                                                             env!("CARGO_PKG_VERSION"))
+                                                         .to_string()));
+                        res.headers_mut().set(ContentLanguage(vec![qitem(LanguageTag{
 							language  : Some("en-GB".to_string()),
 							extlangs  : vec![],
 							script    : None,
@@ -118,60 +131,81 @@ impl Handler for ClientHandler {
 							extensions: BTreeMap::new(),
 							privateuse: vec![],
 						})]));
-						res.headers_mut().set(CacheControl(vec![
+                        res.headers_mut().set(CacheControl(vec![
 							CacheDirective::Public,
 							CacheDirective::MaxAge(60 * 60 * 24 * 7),
 						]));
-						StatusCode::Ok
-					},
-					Method::Trace => {
-						match u64::from_json_string(&reqbody) {
-							Ok(id) => {
-								let messages = self.messages.read().unwrap();
-								let msgs     = messages.iter().rev().take_while(|&m| m.id != id).collect::<Vec<&_>>()
-								                       .iter().rev().map(|&m| m.clone()).collect::<Vec<_>>();
-								match msgs.to_json_string() {
-									Ok(msgs) => {
-										body = Some(msgs);
-										StatusCode::Ok
-									},
-									Err(error) => {
-										let _ = stderr().write_fmt(format_args!("Couldn't create a JSON response for {}: {}\n", req.remote_addr, error));
-										body = Some("[]".to_string());  // Empty array
-										StatusCode::Accepted
-									},
-								}
-							},
-							Err(error) => {
-								let _ = stderr().write_fmt(format_args!("Couldn't process a TRACE message id ({:?}) from {}: {}\n", reqbody, req.remote_addr, error));
-								StatusCode::UnprocessableEntity
-							},
-						}
-					},
-				  _ => StatusCode::ImATeapot,
-				},
-			Err(error) => {
-				let _ = stderr().write_fmt(format_args!("Failed reading request from {}: {}\n", req.remote_addr, error));
-				StatusCode::UnsupportedMediaType  // non-UTF-8
-			},
-		};
+                        StatusCode::Ok
+                    }
+                    Method::Trace => {
+                        match u64::from_json_string(&reqbody) {
+                            Ok(id) => {
+                                let messages = self.messages.read().unwrap();
+                                let msgs = messages.iter()
+                                                   .rev()
+                                                   .take_while(|&m| m.id != id)
+                                                   .collect::<Vec<&_>>()
+                                                   .iter()
+                                                   .rev()
+                                                   .map(|&m| m.clone())
+                                                   .collect::<Vec<_>>();
+                                match msgs.to_json_string() {
+                                    Ok(msgs) => {
+                                        body = Some(msgs);
+                                        StatusCode::Ok
+                                    }
+                                    Err(error) => {
+                                        let _ = stderr()
+                                                    .write_fmt(format_args!("Couldn't create a \
+                                                                             JSON response for \
+                                                                             {}: {}\n",
+                                                                            req.remote_addr,
+                                                                            error));
+                                        body = Some("[]".to_string());  // Empty array
+                                        StatusCode::Accepted
+                                    }
+                                }
+                            }
+                            Err(error) => {
+                                let _ = stderr().write_fmt(format_args!("Couldn't process a \
+                                                                         TRACE message id \
+                                                                         ({:?}) from {}: {}\n",
+                                                                        reqbody,
+                                                                        req.remote_addr,
+                                                                        error));
+                                StatusCode::UnprocessableEntity
+                            }
+                        }
+                    }
+                    _ => StatusCode::ImATeapot,
+                }
+            }
+            Err(error) => {
+                let _ = stderr().write_fmt(format_args!("Failed reading request from {}: {}\n",
+                                                        req.remote_addr,
+                                                        error));
+                StatusCode::UnsupportedMediaType  // non-UTF-8
+            }
+        };
 
 
-		let handle_error = |error: Result<(), io::Error>| {
-			if let Err(error) = error {
-				let _ = stderr().write_fmt(format_args!("Failed to respond to {}: {}", req.remote_addr, error));
-			}
-		};
+        let handle_error = |error: Result<(), io::Error>| {
+            if let Err(error) = error {
+                let _ = stderr().write_fmt(format_args!("Failed to respond to {}: {}",
+                                                        req.remote_addr,
+                                                        error));
+            }
+        };
 
-		let reply = |body: &String| {
-			res.headers_mut().set(ContentLength(body.len() as u64));
-			handle_error(res.start().unwrap().write_all(body.as_bytes()));
-		};
+        let reply = |body: &String| {
+            res.headers_mut().set(ContentLength(body.len() as u64));
+            handle_error(res.start().unwrap().write_all(body.as_bytes()));
+        };
 
-		match (body, body_ref) {
-			(Some(body), _)     => reply(&body),
-			(_, Some(ref body)) => reply(&body),
-			(None, None)        => reply(&"".to_string()),
-		}
-	}
+        match (body, body_ref) {
+            (Some(body), _) => reply(&body),
+            (_, Some(ref body)) => reply(&body),
+            (None, None) => reply(&"".to_string()),
+        }
+    }
 }
--- src/options.rs
+++ src/options.rs
@@ -10,109 +10,128 @@ use yaml_file_handler::yaml_handler::FileHandler as YamlFileHandler;
 
 #[derive(Debug)]
 pub struct Options {
-	pub port: u16,
-	pub ssl: Option<(String, String)>,
+    pub port: u16,
+    pub ssl: Option<(String, String)>,
 }
 
 
 impl Options {
-	/// Parses commandline arguments into an [`Options`](#) instance
-	///
-	/// Optionally reads from a config file in [YAML](http://yaml.org) format, however commandline arguments take preference thereover.
-	/// The config file format is non-trivial, see `"example/config.yml"`.
-	pub fn parse() -> Options {
-		const PORT_USAGE: &'static str = "-p --port [PORT]          'Specifies the port the server will run on, will prompt if not specified'";
-		const USAGE:      &'static str = "--ssl [SSL_CERT;SSL_KEY]  'Sets SSL cert and key to use
-		                                  -c --config=[CONFIG_FILE] 'Sets config file to load, values will be overriden by commandline args'";
+    /// Parses commandline arguments into an [`Options`](#) instance
+    ///
+    /// Optionally reads from a config file in [YAML](http://yaml.org) format, however commandline arguments take preference thereover.
+    /// The config file format is non-trivial, see `"example/config.yml"`.
+    pub fn parse() -> Options {
+        const PORT_USAGE: &'static str = "-p --port [PORT]          'Specifies the port the \
+                                          server will run on, will prompt if not specified'";
+        const USAGE: &'static str = "--ssl [SSL_CERT;SSL_KEY]  'Sets SSL cert and key to use
+		                                  \
+                                     -c --config=[CONFIG_FILE] 'Sets config file to load, values \
+                                     will be overriden by commandline args'";
 
-		let matches = Clapp::new("chattium-oxide-server").version(env!("CARGO_PKG_VERSION"))
-		                                                 .author("nabijaczleweli <nabijaczleweli@gmail.com>")
-		                                                 .about("Chat server for chattium-oxide-client")
-		                                                 .args_from_usage(USAGE)
-		                                                 .arg(Clarg::from_usage(PORT_USAGE).validator(|val| {
-		                                                 	match u16::from_str(&*&val) {
-		                                                 		Ok(_) => Ok(()),
-		                                                 		Err(error) => Err(error.description().to_string()),
-		                                                 	}
-		                                                 }))
-		                                                 .get_matches();
-		let mut port    : Option<u16>    = None;
-		let mut ssl_cert: Option<String> = None;
-		let mut ssl_key : Option<String> = None;
+        let matches = Clapp::new("chattium-oxide-server")
+                          .version(env!("CARGO_PKG_VERSION"))
+                          .author("nabijaczleweli <nabijaczleweli@gmail.com>")
+                          .about("Chat server for chattium-oxide-client")
+                          .args_from_usage(USAGE)
+                          .arg(Clarg::from_usage(PORT_USAGE).validator(|val| {
+                              match u16::from_str(&*&val) {
+                                  Ok(_) => Ok(()),
+                                  Err(error) => Err(error.description().to_string()),
+                              }
+                          }))
+                          .get_matches();
+        let mut port: Option<u16> = None;
+        let mut ssl_cert: Option<String> = None;
+        let mut ssl_key: Option<String> = None;
 
-		if let Some(config) = matches.value_of("config") {
-			let mut yaml = YamlFileHandler::new();
-			if yaml.add_files(vec![config]) {
-				if let Some(yaml) = yaml.read_all_files().as_ref().map(|all| {
-					let mut b = PathBuf::from(config);
-					b.set_extension("");
-					&all[b.file_name().unwrap().to_str().unwrap()]
-				}) {
-					if let Some(map) = yaml["ssl"].as_hash() {
-						ssl_cert = if let Some(cert) = map.get(&Yaml::String("cert".to_string())) {cert.as_str().map(|c| c.to_string())} else {None};
-						ssl_key  = if let Some(key)  = map.get(&Yaml::String("key".to_string()))  {key .as_str().map(|k| k.to_string())} else {None};
-					}
-					port = yaml["port"].as_i64().map(|p| p as u16);
-				}
-			}
-		}
+        if let Some(config) = matches.value_of("config") {
+            let mut yaml = YamlFileHandler::new();
+            if yaml.add_files(vec![config]) {
+                if let Some(yaml) = yaml.read_all_files().as_ref().map(|all| {
+                    let mut b = PathBuf::from(config);
+                    b.set_extension("");
+                    &all[b.file_name().unwrap().to_str().unwrap()]
+                }) {
+                    if let Some(map) = yaml["ssl"].as_hash() {
+                        ssl_cert = if let Some(cert) = map.get(&Yaml::String("cert".to_string())) {
+                            cert.as_str().map(|c| c.to_string())
+                        } else {
+                            None
+                        };
+                        ssl_key = if let Some(key) = map.get(&Yaml::String("key".to_string())) {
+                            key.as_str().map(|k| k.to_string())
+                        } else {
+                            None
+                        };
+                    }
+                    port = yaml["port"].as_i64().map(|p| p as u16);
+                }
+            }
+        }
 
-		if let Some(cport) = matches.value_of("port") {port = Some(u16::from_str(cport).unwrap())}  // Validated using Arg::validator
-		if let Some(cssl) = matches.values_of("ssl") {
-			let cssl = cssl.collect::<Vec<_>>().join(";");
-			let mut paths = cssl.split(';');
-			ssl_cert = Some(paths.next().unwrap().to_string());
-			ssl_key  = Some(paths.next().unwrap().to_string());
-		}
+        if let Some(cport) = matches.value_of("port") {
+            port = Some(u16::from_str(cport).unwrap())
+        }  // Validated using Arg::validator
+        if let Some(cssl) = matches.values_of("ssl") {
+            let cssl = cssl.collect::<Vec<_>>().join(";");
+            let mut paths = cssl.split(';');
+            ssl_cert = Some(paths.next().unwrap().to_string());
+            ssl_key = Some(paths.next().unwrap().to_string());
+        }
 
-		if port.is_none() {
-			let mut tport: Option<u16> = None;
-			while tport.is_none() {
-				match read_prompted(format_args!("No port specified.\nPlease type in the port now: ")) {
-					Ok(rport) =>
-						match rport {
-							Some(rport) =>
-								match u16::from_str(&*&rport) {
-									Ok(port) => tport = Some(port),
-									Err(error) => println!("Invalid port value: {}", error),
-								},
-							None => (),
-						},
-					Err(error) => println!("Couldn't read port: {}", error),
-				}
-			}
-			port = tport;
-		}
+        if port.is_none() {
+            let mut tport: Option<u16> = None;
+            while tport.is_none() {
+                match read_prompted(format_args!("No port specified.\nPlease type in the port \
+                                                  now: ")) {
+                    Ok(rport) => {
+                        match rport {
+                            Some(rport) => {
+                                match u16::from_str(&*&rport) {
+                                    Ok(port) => tport = Some(port),
+                                    Err(error) => println!("Invalid port value: {}", error),
+                                }
+                            }
+                            None => (),
+                        }
+                    }
+                    Err(error) => println!("Couldn't read port: {}", error),
+                }
+            }
+            port = tport;
+        }
 
-		assert!(port.is_some());
-		Options{
-			port: port.unwrap(),
-			ssl: match (ssl_cert, ssl_key) {
-				(Some(cert), Some(key)) => Some((cert, key)),
-				(None, Some(key)) => {
-					println!("SSL key specified (\"{}\"), but certificate not specified", key);
-					None
-				},
-				(Some(cert), None) => {
-					println!("SSL key not specified, but certificate specified (\"{}\")", cert);
-					None
-				},
-				(None, None) => None,
-			},
-		}
-	}
+        assert!(port.is_some());
+        Options {
+            port: port.unwrap(),
+            ssl: match (ssl_cert, ssl_key) {
+                (Some(cert), Some(key)) => Some((cert, key)),
+                (None, Some(key)) => {
+                    println!("SSL key specified (\"{}\"), but certificate not specified",
+                             key);
+                    None
+                }
+                (Some(cert), None) => {
+                    println!("SSL key not specified, but certificate specified (\"{}\")",
+                             cert);
+                    None
+                }
+                (None, None) => None,
+            },
+        }
+    }
 }
 
 
 fn read_prompted(prompt: FormatArguments) -> io::Result<Option<String>> {
-	try!(io::stdout().write_fmt(prompt));
-	try!(io::stdout().flush());
+    try!(io::stdout().write_fmt(prompt));
+    try!(io::stdout().flush());
 
-	let mut obuf = String::new();
-	try!(io::stdin().read_line(&mut obuf));  // Don't match on this directly, because it returns with "\r\n"
-	let obuf = obuf.trim();
-	Ok(match obuf.len() {
-		0 => None,
-		_ => Some(obuf.trim().to_string()),
-	})
+    let mut obuf = String::new();
+    try!(io::stdin().read_line(&mut obuf));  // Don't match on this directly, because it returns with "\r\n"
+    let obuf = obuf.trim();
+    Ok(match obuf.len() {
+        0 => None,
+        _ => Some(obuf.trim().to_string()),
+    })
 }
